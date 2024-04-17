d9753a8564e4b27f49226427c7a997c224f36255
John Harvardalinayu03 <alinayu@college.harvard.edu>
2024-04-17 16:52:52 -0400

    setup

---
+++ numpart.py
@@ -0,0 +1,50 @@
+import sys
+
+def main():
+    flag = sys.argv[1]
+    algorithm_code = sys.argv[2]
+    input_file = sys.argv[3]
+
+    try:
+        with open(input_file, 'r') as file:
+            data = file.read()
+    except FileNotFoundError:
+        print(f"Error: The file {input_file} does not exist.")
+        sys.exit(1)
+
+    if algorithm_code == 0:
+        karmarkar_karp(data)
+    elif algorithm_code == 1:
+        repeated_random(data)
+    elif algorithm_code == 2:
+        hill_climbing(data)
+    elif algorithm_code == 11:
+        prepartitioned_RR(data)
+    elif algorithm_code == 12:
+        prepartitioned_HC(data)
+    elif algorithm_code == 13:
+        prepartitioned_SA(data)
+
+def karmarkar_karp(data):
+    return 
+
+def repeated_random(data):
+    return
+
+def hill_climbing(data):
+    return
+
+def simulated_annealing(data):
+    return
+
+def prepartitioned_RR(data):
+    return
+
+def prepartitioned_HC(data):
+    return
+
+def prepartitioned_SA(data):
+    return
+
+if __name__ == "__main__":
+    main()
