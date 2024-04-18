6c077b892964b6fa77709c67df4ec9fe39aea805
John Harvardalinayu03 <alinayu@college.harvard.edu>
2024-04-18 17:18:31 -0400

    signature

--- numpart.py
+++ numpart.py
@@ -1,11 +1,13 @@
 import sys
 import heapq
+import random
 
 def main():
     flag = sys.argv[1]
     algorithm_code = int(sys.argv[2])
     input_file = sys.argv[3]
     data = []
+    max_iter = 50
 
     try:
         with open(input_file, 'r') as file:
@@ -18,15 +20,15 @@ def main():
     if algorithm_code == 0:
         karmarkar_karp(data)
     elif algorithm_code == 1:
-        repeated_random(data)
+        repeated_random(data, max_iter)
     elif algorithm_code == 2:
-        hill_climbing(data)
+        hill_climbing(data, max_iter)
     elif algorithm_code == 11:
-        prepartitioned_RR(data)
+        prepartitioned_RR(data, max_iter)
     elif algorithm_code == 12:
-        prepartitioned_HC(data)
+        prepartitioned_HC(data, max_iter)
     elif algorithm_code == 13:
-        prepartitioned_SA(data)
+        prepartitioned_SA(data, max_iter)
 
 def karmarkar_karp(data):
     max_heap = [-x for x in data]
@@ -44,22 +46,21 @@ def karmarkar_karp(data):
     return(residue)
 
 
-def repeated_random(data):
+def repeated_random(data, max_iter):
+
+def hill_climbing(data, max_iter):
     return
 
-def hill_climbing(data):
+def simulated_annealing(data, max_iter):
     return
 
-def simulated_annealing(data):
+def prepartitioned_RR(data, max_iter):
     return
 
-def prepartitioned_RR(data):
+def prepartitioned_HC(data, max_iter):
     return
 
-def prepartitioned_HC(data):
-    return
-
-def prepartitioned_SA(data):
+def prepartitioned_SA(data, max_iter):
     return
 
 if __name__ == "__main__":
